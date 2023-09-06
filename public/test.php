d4b4b81f92e2cdae7ad8165e8b93b0b04ada16b8
Phuong Nguyen <phuongnguyen@gearment.com>
2023-09-06 15:08:25 +0700

    rabbitmq

--- public/test.php
+++ public/test.php
@@ -1,2 +1,12 @@
 <?php
-echo "Test";
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitusr', 'rabbitpw');
+} catch (Exception $e) {
+    dd($e->getMessage());
+}
+$channel = $connection->channel();
+dd($channel->getChannelId());
