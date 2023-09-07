151bf7fef3d71bf9bd0f3954287ab1e9ed796e3a
Phuong Nguyen <phuongnguyen@gearment.com>
2023-09-07 16:16:55 +0700

    rabbitmq

---
+++ public/receive.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitusr', 'rabbitpw');
+    $channel = $connection->channel();
+
+    $channel->queue_declare('greeting', false, false, false, false);
+
+    echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+    $callback = function ($message) {
+        echo "[x] Received message ", $message->getBody(), "\n";
+    };
+
+    $channel->basic_consume('greeting', '', false, true, false, false, $callback);
+
+    while ($channel->is_open()) {
+        $channel->wait();
+    }
+
+    $channel->close();
+    $connection->close();
+} catch (Exception $e) {
+    dd($e->getMessage());
+}
---
+++ public/send.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitusr', 'rabbitpw');
+    $channel = $connection->channel();
+
+    $channel->queue_declare('greeting', false, false, false, false);
+
+    $msgBody = 'Hi there: ' . date('c');
+    $msg = new AMQPMessage($msgBody);
+
+    $channel->basic_publish($msg, '', 'greeting');
+
+    echo "Sent message: <<$msgBody>>";
+    $channel->close();
+    $connection->close();
+} catch (Exception $e) {
+    dd($e->getMessage());
+}
