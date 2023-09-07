70cac4b0c7c1f59dd336d2f2b6f74663d713e858
Phuong Nguyen <phuongnguyen@gearment.com>
2023-09-07 16:37:29 +0700

    rabbitmq

---
+++ rabbitmq/new_task.php
@@ -0,0 +1,26 @@
+<?php
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitusr', 'rabbitpw');
+    $channel = $connection->channel();
+
+    $channel->queue_declare('greeting', false, false, false, false);
+
+    $data = implode(' ', array_slice($argv, 1));
+    if (empty($data)) {
+        $data = 'Hi there: ' . date('c');
+    }
+
+    $msg = new AMQPMessage($data);
+
+    $channel->basic_publish($msg, '', 'greeting');
+
+    echo "Sent message: <<$data>>";
+    $channel->close();
+    $connection->close();
+} catch (Exception $e) {
+    dd($e->getMessage());
+}
