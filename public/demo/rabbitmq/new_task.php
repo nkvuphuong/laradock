ecb8e8e085d7da295b8c3220096111817bc1029b
Phuong Nguyen <phuongnguyen@gearment.com>
2023-09-18 14:51:11 +0700

    Rabbit MQ: durable and Fair dispatch

--- public/demo/rabbitmq/new_task.php
+++ public/demo/rabbitmq/new_task.php
@@ -1,24 +1,24 @@
 <?php
-require_once '../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 try {
+    $queueName = 'task_queue';
     $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitusr', 'rabbitpw');
     $channel = $connection->channel();
 
-    $channel->queue_declare('greeting', false, false, false, false);
+    $durable = true; //make sure that the queue will survive a RabbitMQ node restart
+    $channel->queue_declare($queueName, false, $durable, false, false);
 
-    $data = implode(' ', array_slice($argv, 1));
-    if (empty($data)) {
-        $data = 'Hi there: ' . date('c');
+    for ($i = 0; $i < 100; $i++) {
+        $data = 'Task No#' . $i;
+        $msg = new AMQPMessage($data, array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+        $channel->basic_publish($msg, '', $queueName);
+        echo "Sent message: <<$data>> \n";
     }
 
-    $msg = new AMQPMessage($data);
-
-    $channel->basic_publish($msg, '', 'greeting');
-
-    echo "Sent message: <<$data>>";
     $channel->close();
     $connection->close();
 } catch (Exception $e) {
--- public/demo/rabbitmq/worker.php
+++ public/demo/rabbitmq/worker.php
@@ -1,24 +1,30 @@
 <?php
-require_once '../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 try {
+    $queueName = 'task_queue';
     $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitusr', 'rabbitpw');
     $channel = $connection->channel();
 
-    $channel->queue_declare('greeting', false, false, false, false);
+    $durable = true; //make sure that the queue will survive a RabbitMQ node restart
+    $channel->queue_declare($queueName, false, $durable, false, false);
 
     echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
-    $callback = function (PhpAmqpLib\Message\AMQPMessage $msg) {
-        echo ' [x] Received ', $msg->body, "\n";
-        sleep(substr_count($msg->body, '.'));
-        echo " [x] Done\n";
+    $callback = static function (PhpAmqpLib\Message\AMQPMessage $msg) {
+        $sleepingTime = rand(1, 5);
+        echo " [x] Received: waiting for $sleepingTime seconds ", $msg->body, "\n";
+        sleep($sleepingTime);
+        echo " [x] Done: " . date('c') . "\n";
         $msg->ack();
     };
 
-    $channel->basic_consume('greeting', '', false, false, false, false, $callback);
+    $channel->basic_qos(null, 1, null); // (Fair dispatch) This tells RabbitMQ not to give more than one message to a worker at a time. Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one. Instead, it will dispatch it to the next worker that is not still busy.
+
+    $channel->basic_consume($queueName, '', false, false, false, false, $callback);
 
     while ($channel->is_open()) {
         $channel->wait();
